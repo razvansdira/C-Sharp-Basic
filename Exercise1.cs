75b7bfec317d2cd5afcada019ee2221f7ac56ce1
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-01 12:01:47 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello: ");
+            Console.WriteLine("Razvan Sdira");
+            Console.ReadKey();
+        }
+    }
+}
