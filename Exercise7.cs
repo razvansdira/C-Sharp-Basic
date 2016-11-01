a6e42f1972ac1af8a2a256f1727b021691316030
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-01 13:27:01 +0200

    Add files via upload

---
+++ Exercise7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the first number: ");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the second number: ");
+            int nr2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Operations: ");
+
+            Console.WriteLine("{0} + {1} = {2}", nr1, nr2, nr1 + nr2);
+            Console.WriteLine("{0} - {1} = {2}", nr1, nr2, nr1 - nr2);
+            Console.WriteLine("{0} x {1} = {2}", nr1, nr2, nr1 * nr2);
+            Console.WriteLine("{0} / {1} = {2}", nr1, nr2, nr1 / nr2);
+            Console.WriteLine("{0} mod {1} = {2}", nr1, nr2, nr1 % nr2);
+            Console.ReadKey();
+        }
+    }
+}
