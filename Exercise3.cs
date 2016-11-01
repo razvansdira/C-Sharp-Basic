9d35c406d4e6648807d0750d3bd4b35239f5e843
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-01 12:14:36 +0200

    Add files via upload

---
+++ Exercise3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(16 / 2);
+            Console.ReadKey();
+        }
+    }
+}
