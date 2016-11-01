4be0949149d13379188ac1af1f9020fe06b1050c
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-01 12:56:30 +0200

    Add files via upload

---
+++ Exercise6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise6
+    {
+        static void Main(string[] args)
+        {
+            int nr1, nr2, nr3;
+
+            Console.WriteLine("Input the first number to multiply: ");
+            nr1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the second number to multiply: ");
+            nr2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the third number to multiply: ");
+            nr3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Output:");
+            Console.WriteLine(nr1 * nr2 * nr3);
+            Console.ReadKey();
+        }
+    }
+}
