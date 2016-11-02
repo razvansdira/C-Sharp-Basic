5eaef5eb91b6eca7b67aed03b889394f0c9aee93
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-02 12:18:19 +0200

    Add files via upload

---
+++ Exercise11.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise11
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("Enter your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You look older than {0}, sorry!", age);
+            Console.ReadKey();
+        }
+    }
+}
