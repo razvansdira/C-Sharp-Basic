9724362f528e985004e5975b8f42d8b2506c3e52
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-02 12:38:49 +0200

    Add files via upload

---
+++ Exercise13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise13
+    {
+        static void Main (String[] args)
+        {
+            int nr;
+
+            Console.WriteLine("Enter a number: ");
+            nr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Display:");
+            Console.WriteLine("{0}{0}{0}\n", nr);
+            Console.WriteLine("{0} {0}\n", nr);
+            Console.WriteLine("{0} {0}\n", nr);
+            Console.WriteLine("{0} {0}\n", nr);
+            Console.WriteLine("{0}{0}{0}", nr);
+
+            Console.ReadKey();
+        }
+    }
+}
