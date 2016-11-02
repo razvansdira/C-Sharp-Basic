a4940201c4a7d5e22b3a8a1e29164c2c47704822
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-02 12:45:35 +0200

    Add files via upload

---
+++ Exercise14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic
+{
+    class Exercise14
+    {
+        static void Main(string[] args)
+        {
+            int c;
+
+            Console.WriteLine("Enter the amount of celsius: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", c+273);
+            Console.WriteLine("Fahrenheit = {0}", c*18/10+32);
+
+            Console.ReadKey();
+        }
+    }
+}
